b5a78201c97e6256116a60d93e9bacbd1ee26d2e
sanjay <sanjayshrestha772@gmail.com>
2024-08-22 19:42:20 +0545

    file initialized

---
+++ script.js
@@ -0,0 +1,7 @@
+const myDiv=document.getElementById('myDiv');
+// const newDiv=document.createElement('div');
+// newDiv.textContent='Not so good';
+// newDiv.style.backgroundColor='red'
+// myDiv.insertAdjacentElement('beforeend',newDiv)
+
+myDiv.insertAdjacentText('beforeend','Hey wrodl')
